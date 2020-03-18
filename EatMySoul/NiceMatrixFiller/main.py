778ba27cd6ec80223da02a1887f44b0277fb73be
EatMySoul <58660230+EatMySoul@users.noreply.github.com>
2020-03-18 21:21:31 +0300

    its worked

--- EatMySoul/NiceMatrixFiller/main.py
+++ EatMySoul/NiceMatrixFiller/main.py
@@ -3,11 +3,19 @@ import os
 
 
 def matrix_input(num):
+    matrix[position_y][position_x] = num
     pass
 
 
 def show_matrix():
-    print(position_x , position_y)
+    for i in range(lines):
+        for j in range(columns):
+            if position_x == j and position_y == i:
+                print('\033[32m',matrix[i][j],'\033[0m',end = '',sep='')
+            else:    
+                print(matrix[i][j],end = '')
+        print()
+    print(position_x, position_y)
 
 
 def start_listener():
@@ -15,18 +23,26 @@ def start_listener():
         listener.join()
 
 def on_press(key):
-    global position_x , position_y
+    global position_x , position_y, matrix, lines, columns
     os.system('clear')
+
     if key == keyboard.Key.enter:
         pass
     elif key == keyboard.Key.right:
+        if (position_x + 1) == columns:
+            for i in range(lines):
+                matrix[i].append(0)
+            columns += 1
         position_x += 1
     elif key == keyboard.Key.left:
         if position_x > 0:
             position_x -= 1
-    elif key == keyboard.Key.up:
-        position_y += 1
     elif key == keyboard.Key.down:
+        if (position_y + 1) == lines:
+            matrix.append([0]*columns)
+            lines += 1
+        position_y += 1
+    elif key == keyboard.Key.up:
         if position_y > 0:
             position_y -= 1
     else:
@@ -38,7 +54,11 @@ def on_press(key):
 
 
 def main():
-    global position_x, position_y
+    global position_x , position_y, matrix, lines, columns
+
+    lines = 2
+    columns = 2
+    matrix = [[0]*lines for i in range(columns)]
     position_x = 0
     position_y = 0
     start_listener()
